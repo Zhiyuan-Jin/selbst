8f11ede8ace312ea8278f02df9269c913e188e66
go97hoc <go97hoc@mytum.de>
2026-06-23 20:37:35 +0200

    1

--- src/main/java/de/tum/in/ase/eist/UniversityApp.java
+++ src/main/java/de/tum/in/ase/eist/UniversityApp.java
@@ -40,7 +40,7 @@ public class UniversityApp extends Application {
 
     public String getButtonText() {
         // TODO 1: Fix the returned String
-        return "Hello World";
+        return "Hello EIST";
     }
 
     public static void startApp(String[] args) {
